23292c0a3e3cd9057e97b9ed519deb77289dc5f7
fmaric77 <fmaric77@gmail.com>
2024-12-15 16:10:13 +0100

    ažuriranje statusa putovanja na strani vozača

--- app/vozac/page.tsx
+++ app/vozac/page.tsx
@@ -21,7 +21,7 @@ const formatDate = (dateString: string) => {
   date.setDate(date.getDate());
 
   const day = date.getDate().toString().padStart(2, '0');
-  const month = (date.getMonth()).toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const year = date.getFullYear();
 
   return `${day}.${month}.${year}`;
